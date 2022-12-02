9c647b74a8156b154907aff653436bc51bfac577
yphsieh <melosyhsieh44@gmail.com>
2022-12-02 13:15:06 +0800

    detector

--- src/send_script/send_script/shapes.py
+++ src/send_script/send_script/shapes.py
@@ -75,7 +75,7 @@ def detect(img: np.ndarray) -> List[Frame2D]:
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    _, thresh = cv2.threshold(blurred, 128, 255, cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)
     _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # loop over the contours
