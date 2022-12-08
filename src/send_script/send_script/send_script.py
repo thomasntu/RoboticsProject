80355fb5ba580bc1a11a34a6d868928d5d8c48f9
thommann <thomasrolf.mannhart@uzh.ch>
2022-12-08 21:57:37 +0800

    face drawing robot control

--- src/send_script/send_script/send_script.py
+++ src/send_script/send_script/send_script.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 import time
-from tqdm import tqdm
 from os import remove
 from os.path import exists
-from typing import List
 
 import cv2
 import numpy as np
 import rclpy
+from tqdm import tqdm
 
 from . import shapes
 from .controller import send_script
@@ -30,8 +29,8 @@ def go_to_point(p, z=227):
     go_to(p[0], p[1], z)
 
 
-def go_to(x, y, z):
-    target_p1 = f"{x}, {y}, {z}, -180.00, 0.0, 135.00"
+def go_to(x, y, z, a: float = -180.0, b: float = 0.0, c: float = 135.0):
+    target_p1 = f"{x}, {y}, {z}, {a:.2f}, {b:.2f}, {c:.2f}"
     script1 = "PTP(\"CPP\"," + target_p1 + ",100,200,0,false)"
     send_script(script1)
 
@@ -40,7 +39,7 @@ def take_picture():
     send_script("Vision_DoJob(job1)")
 
 
-def loop():
+def copy_image_loop():
     """
     Copy image
     """
@@ -83,6 +82,63 @@ def loop():
         elif time.time() - start > 120:
             break
 
+
+def draw_face_loop():
+    """
+    Draw Face
+    """
+
+    # Take a picture of the face
+    go_to(350, 350, 730, 0, 0, 0)
+    take_picture()
+
+    start = time.time()
+    filename = f'images/IMG.png'
+
+    images = []
+
+    while True:
+        if exists(filename):
+            time.sleep(1)
+
+            image = cv2.imread(filename)
+            remove(filename)
+
+            images.append(image)
+
+        if len(images) == 2:
+
+            # Go to a point close to the table while computing the path
+            go_to(350, 350, 300)
+
+            # First image is canvas, second image is face
+            path_nodes, jump_nodes = shapes.calculate_path_for_face(images[1], images[0])
+
+            # TODO: Modify return type in calculate_path()
+            # such that `if prev_node in jump_nodes` is valid
+            path_nodes = np.array(path_nodes)
+            jump_nodes = np.array(jump_nodes)
+
+            prev_node = path_nodes[0]
+
+            # Go to the first node
+            go_to_point(prev_node, z=300)
+            go_to_point(prev_node)
+
+            for path_node in tqdm(path_nodes):
+                if prev_node in jump_nodes:
+                    jump(prev_node, path_node)
+                else:
+                    go_to_point(path_node)
+                prev_node = path_node
+
+            # return to initial position
+            go_to(350, 350, 730)
+
+        elif time.time() - start > 120:
+            break
+
+
 def main(args=None):
     rclpy.init(args=args)
 
@@ -91,7 +147,7 @@ def main(args=None):
     take_picture()
 
     # Enter into the main loop
-    loop()
+    copy_image_loop()
 
     # Shutdown
     rclpy.shutdown()
